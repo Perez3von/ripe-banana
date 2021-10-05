670ad1459a44efebdf7401d36f742aac88d1fa04
Evon Perez <perez.evon@gmail.com>
2021-10-05 19:01:42 -0400

    post, insert film test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,7 +4,8 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 //PATH to DATA FILE as variable. 
 const dummyStudioData = require('../lib/dummyData/dummyStudioData.js');
-const faker = require('faker');
+const dummyFilmData = require('../lib/dummyData/dummyFilmData.js');
+
 
 
 describe('ripe-banana routes', () => {
@@ -18,7 +19,7 @@ describe('ripe-banana routes', () => {
   it('POST /studios returns array of objects inserted', async () => {
     const single_object_from_dummyData = dummyStudioData[0];
 
-    const res = await request(app).post('/studios/').send(single_object_from_dummyData);
+    const res = await request(app).post('/studios').send(single_object_from_dummyData);
    
 
     expect(res.body).toEqual(single_object_from_dummyData);
@@ -28,45 +29,56 @@ describe('ripe-banana routes', () => {
 
   //-----------------------------------------------------------------------------------------------------/
 
-  it('GET /studios, returns [{ id, name }]', async() => {
-    const res = await request(app).get('/studios/');
+  
+  it('POST /films returns array of objects inserted', async () => {
+    const single_object_from_dummyData = dummyFilmData[0];
+console.log('LOOOL', single_object_from_dummyData);
+    const res = await request(app).post('/films').send(single_object_from_dummyData);
+   
+    expect(res.body).toEqual(single_object_from_dummyData);
+
+  });
+
+
+  //-----------------------------------------------------------------------------------------------------/
+
+  it('GET /studios, returns [{ id, name }]', async () => {
+    const res = await request(app).get('/studios');
     expect(res.body).toEqual([{
 
       id: expect.any(Number),
       name: expect.any(String) 
       
-    }])
+    }]);
   });
-  // console.log('AAAAAHHHHHHH!!!!!!!!', faker.date);
-  
-  });
+});
 
 
-  //-----------------------------------------------------------------------------------------------------/
+//-----------------------------------------------------------------------------------------------------/
 
-  // it('GETS studio by ID', async => {
-  //   const res = await request(app).get('/studios/1');
-  //   expect(res.body).toEqual({
+// it('GETS studio by ID', async => {
+//   const res = await request(app).get('/studios/1');
+//   expect(res.body).toEqual({
 
-  //     id: expect.any(Number),
-  //     name: expect.any(String),
-  //     city: expect.any(String),
-  //     state: expect.any(String),
-  //     country: expect.any(String),
-  //     films: [{
+//     id: expect.any(Number),
+//     name: expect.any(String),
+//     city: expect.any(String),
+//     state: expect.any(String),
+//     country: expect.any(String),
+//     films: [{
 
-  //     id: expect.any(Number),
-  //     title: expect.any(String)
+//     id: expect.any(Number),
+//     title: expect.any(String)
         
-  //     }]
-  //   })
-  // })
+//     }]
+//   })
+// })
 
 
 
 
-  //-----------------------------------------------------------------------------------------------------/
-  afterAll(() => {
-    pool.end();
-  });
+//-----------------------------------------------------------------------------------------------------/
+afterAll(() => {
+  pool.end();
+});
 
