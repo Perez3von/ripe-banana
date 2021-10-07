7ca454d9bf3a90bbae4608513223aa6129645c5b
Evon Perez <perez.evon@gmail.com>
2021-10-06 21:41:50 -0400

    fixing closing braces

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -100,83 +100,83 @@ describe('ripe-banana routes', () => {
       
     }]);
   });
-});
 
 
-//-----------------------------------------------------------------------------------------------------/
 
-it('GETS studio by ID', async () => {
-  const res = await request(app).get('/studios/1');
-  expect(res.body).toEqual({
+  //-----------------------------------------------------------------------------------------------------/
 
-    id: expect.any(Number),
-    name: expect.any(String),
-    city: expect.any(String),
-    state: expect.any(String),
-    country: expect.any(String),
-    films: [{
+  it('GETS studio by ID', async () => {
+    const res = await request(app).get('/studios/1');
+    expect(res.body).toEqual({
 
       id: expect.any(Number),
-      title: expect.any(String)
+      name: expect.any(String),
+      city: expect.any(String),
+      state: expect.any(String),
+      country: expect.any(String),
+      films: [{
+
+        id: expect.any(Number),
+        title: expect.any(String)
         
-    }]
+      }]
+    });
   });
-});
 
 
 
 
-//-----------------------------------------------------------------------------------------------------/
+  //-----------------------------------------------------------------------------------------------------/
 
 
-it('GETS films ', async () => {
-  const res = await request(app).get('/films');
-  expect(res.body).toEqual([{
+  it('GETS films ', async () => {
+    const res = await request(app).get('/films');
+    expect(res.body).toEqual([{
 
-    id: expect.any(Number),
-    title: expect.any(String),
-    released: expect.any(Number),
-    studio:{
-      id:expect.any(Number),
-      name:expect.any(String)
-    }
-  }]);
-});
+      id: expect.any(Number),
+      title: expect.any(String),
+      released: expect.any(Number),
+      studio:{
+        id:expect.any(Number),
+        name:expect.any(String)
+      }
+    }]);
+  });
 
-//-----------------------------------------------------------------------------------------------------/
+  //-----------------------------------------------------------------------------------------------------/
 
 
-it('GETS films by ID with actors and reviews ', async () => {
-  const res = await request(app).get('/films/1');
-  expect(res.body).toEqual({
+  it('GETS films by ID with actors and reviews ', async () => {
+    const res = await request(app).get('/films/1');
+    expect(res.body).toEqual({
     //title:expect.any(String),
-    released:expect.any(Number),
-    studio: { id:expect.any(Number), name:expect.any(String) },
-    //cast: [{ id:expect.any(Number), name:expect.any(String) }], // actor id and name
-    reviews: [{
-      id:expect.any(Number),
-      rating:expect.any(String),
-      review:expect.any(String),
-      reviewer: { id:expect.any(Number), name:expect.any(String) }
-    }]
+      released:expect.any(Number),
+      studio: { id:expect.any(Number), name:expect.any(String) },
+      //cast: [{ id:expect.any(Number), name:expect.any(String) }], // actor id and name
+      reviews: [{
+        id:expect.any(Number),
+        rating:expect.any(String),
+        review:expect.any(String),
+        reviewer: { id:expect.any(Number), name:expect.any(String) }
+      }]
+    });
   });
-});
-//---------------------------------------------------------------------------------------------//
+  //---------------------------------------------------------------------------------------------//
   
-it('POSTS data to the table', async () => {
-  const single_object_from_dummyData = dummyFilmsActorsData[0];
+  it('POSTS data to the table', async () => {
+    const single_object_from_dummyData = dummyFilmsActorsData[0];
     
-  const res = await request(app)
-    .post('/filmsactors')
-    .send(single_object_from_dummyData);
-  expect(res.body).toEqual(single_object_from_dummyData);
+    const res = await request(app)
+      .post('/filmsactors')
+      .send(single_object_from_dummyData);
+    expect(res.body).toEqual(single_object_from_dummyData);
+  });
+
+  //-----------------------------------------------------------------------------------------------------/
+
+
+  afterAll(() => {
+    pool.end();
+  });
+//--------------------------------------------------------------------------------------------------------//
 });
-
-//-----------------------------------------------------------------------------------------------------/
-
-
-afterAll(() => {
-  pool.end();
-});
-
-
